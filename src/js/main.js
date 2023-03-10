4c62903beeb366f7f7d22f2cceaca1ea90372d19
skyajax <skyajax@github.com>
2023-03-10 18:32:41 +0200

    fix problems

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,34 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import * as yup from 'yup';
+import onChange from 'on-change';
+
+const userSchema = yup.object({
+    link: url().required()
+});
+
+// export default () => {
+    const form = document.querySelector('form');
+    const input = document.querySelector('.form-control');
+    const state = {
+        linkField: 'valid',
+        value: '',
+    };
+
+    const watchedState = onChange(state, () => {
+
+    })
+
+    // input.addEventListener('change', (e) => {
+    // })
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        console.log(data);
+    })
+// }
+
+console.log('pizza')
